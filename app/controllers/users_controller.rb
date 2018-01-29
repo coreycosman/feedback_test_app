d4dc63d74f402f336bdc9ee2cbf4b5808bf2a2ec
coreycosman <cosman.corey@gmail.com>
2018-01-30 02:27:15 +0400

    wtf?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user.id), notice: "you have successfuly registered"
     else
+      flash.now[:alert] = @user.error.full_messages
       render 'new'
     end
   end
