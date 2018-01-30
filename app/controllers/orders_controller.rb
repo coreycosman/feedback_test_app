7da4006ae8866e47d5a5a75a916a346e970313f5
coreycosman <cosman.corey@gmail.com>
2018-01-30 22:58:30 +0400

    fixed issue re: displaying order form on user show page- 1 missing bracket on orders controller, create action- @user = User.find(params[:id]) was missing closing bracket

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,11 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new
     @order.pickup_start = params[:order][:pickup_start]
-    @user = User.find(params[:id]
-    @user.id = @user.orders
-    @order.user_id = current_user.id
+    @user = User.find(params[:id])
 
     if @order.save
       flash.notice = "Order submitted"
+      redirect_to user_path
     else
       render "users/show"
     end
