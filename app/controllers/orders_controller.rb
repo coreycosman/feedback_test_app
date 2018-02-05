a8249c50821c1da23f8f8c462b6444efabdff2ff
coreycosman <cosman.corey@gmail.com>
2018-02-05 23:09:00 +0400

    in process of updating order table through a claim form that is displayed on the user show page, claim form currently not reaching the order put route

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,31 +2,32 @@ class OrdersController < ApplicationController
 
   def create
 
-    if current_user.role == "corporation"
+   if current_user.role == "corporation"
       @order = Order.new(order_params)
       @order.order_status = "open"
       find_user
       corporation_order_submit?
-
-
-    elsif current_user.role == "driver"
-      @order.order_status == "driver"
-      find_user
-      driver_order_submit?
     end
-
-
   end
 
   def update
-    @order = Order.find_by(params[:id])
+    if current_user.role == "charity"
+      @order = Order.find_by(params[:id])
+      @order.order_status = "charity"
+      find_user
+      charity_order_submit?
+    end
   end
 
 
   private
 
     def order_params
-      params.require(:order).permit %i(date pickup_start pickup_end food_description dropoff_latest driver_pickup_time driver_dropoff_time)
+      params.require(:order).permit %i(date pickup_start pickup_end food_description)
+    end
+
+    def claim_params
+      params.require(:order).permit %i(dropoff_latest)
     end
 
     def corporation_order_submit?
@@ -40,9 +41,9 @@ class OrdersController < ApplicationController
       end
     end
 
-    def driver_order_submit?
-      @order = Order.new(order_params)
-      if @order.save
+    def charity_order_submit?
+
+      if @order.update(claim_params)
         current_user.orders << @order
         flash.notice = "Order claimed"
         redirect_to user_path(@user.id)
